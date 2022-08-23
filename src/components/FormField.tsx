aeda47c38dd7cd1c258e7d9a9e2ca568eddfd7cb
Stefan Butz <stefan260699@gmail.com>
2022-08-23 14:09:57 +0200

    add datepicker

--- src/components/FormField.tsx
+++ src/components/FormField.tsx
@@ -1,29 +1,56 @@
-import { TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
+import { DesktopDatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
 
 import { Validator, firstErrorMessage } from "../util/Validators";
 
 interface FormFieldProps {
     label: string;
-    type?: 'text';
+    type?: 'text' | 'date';
     value: string;
     onChange: (val: string) => void;
     validators: Validator[];
 }
 
-export default function FormField(f : FormFieldProps) {
-    const errorMsg = firstErrorMessage(f.value, f.validators);
+function render(props: FormFieldProps) {
+    const errorMsg = firstErrorMessage(props.value, props.validators);
+    switch (props.type) {
+        case 'date':
+            return (
+                <DesktopDatePicker
+                    label={props.label}
+                    inputFormat="DD.MM.YYYY"
+                    value={props.value}
+                    onChange={(d) => {
+                        if (d !== null && dayjs(d).isValid())
+                            props.onChange(dayjs(d).format("YYYY-MM-DD"));
+                        else
+                            props.onChange("");
+                    }}
+                    renderInput={(params) => <TextField {...params}
+                />}
+              />
+            );
+        case 'text':
+        default:
+            return (
+                <TextField
+                    type={props.type || 'text'}
+                    label={props.label}
+                    value={props.value}
+                    onChange={(e) => { props.onChange(e.target.value); }}
+                    helperText={errorMsg || " "}
+                    error={errorMsg !== null}
+                    required
+                />
+            );
+    }
+}
+
+export default function FormField(props : FormFieldProps) {
     return (
-        <TextField
-            sx={{mx: 1}}
-            type={f.type || 'text'}
-            label={f.label}
-            value={f.value}
-            onChange={(event) => { f.onChange(event.target.value); }}
-            helperText={errorMsg || " "}
-            error={errorMsg !== null}
-            required
-            margin="normal"
-            autoFocus
-        />
+        <Box sx={{p: 1}}>
+            {render(props)}
+        </Box>
     );
 }
