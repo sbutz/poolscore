8a95894899d82ae38a4b86269f7087407a22f189
Stefan Butz <stefan260699@gmail.com>
2022-08-23 10:09:56 +0200

    extract form builder

--- src/components/FormDialog.tsx
+++ src/components/FormDialog.tsx
@@ -1,24 +1,44 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Stack, TextField } from '@mui/material';
 
 import { Validator, firstErrorMessage } from '../util/Validators';
 
-interface FormField {
+interface FormFieldProps {
     label: string;
+    type?: 'text';
     value: string;
     onChange: (val: string) => void;
     validators: Validator[];
 }
 
+function FormField(f : FormFieldProps) {
+    const errorMsg = firstErrorMessage(f.value, f.validators);
+    return (
+        <TextField
+            key={f.label}
+            type={f.type || 'text'}
+            label={f.label}
+            value={f.value}
+            onChange={(event) => { f.onChange(event.target.value); }}
+            helperText={errorMsg}
+            error={errorMsg !== null}
+            required
+            autoFocus
+            margin="dense"
+        />
+    );
+}
+
 interface FormDialogProps {
     open: boolean;
     title: string;
-    fields: FormField[];
+    children?: React.ReactNode;
+    fields?: FormFieldProps[];
     onCancel: () => void;
     onSave: () => void;
 }
 
 function FormDialog(props: FormDialogProps) {
-    const invalid = props.fields.some(f => {
+    const invalid = props.fields?.some(f => {
         return firstErrorMessage(f.value, f.validators) !== null;
     });
 
@@ -28,25 +48,10 @@ function FormDialog(props: FormDialogProps) {
             {props.title}
         </DialogTitle>
         <DialogContent>
-            {props.fields.map(f => {
-                const errorMsg = firstErrorMessage(f.value, f.validators);
-
-                return (
-                <TextField
-                    key={f.label}
-                    type={'text'}
-                    label={f.label}
-                    value={f.value}
-                    onChange={(event) => { f.onChange(event.target.value); }}
-                    helperText={errorMsg}
-                    error={errorMsg !== null}
-                    required
-                    autoFocus
-                    margin="dense"
-                />
-                )
-
-            })}
+            <Stack spacing={3}>
+                {props.fields?.map(f => <FormField {...f}/>)}
+                {props.children}
+            </Stack>
         </DialogContent>
         <DialogActions>
             <Button onClick={props.onCancel}>Abbrechen</Button>
