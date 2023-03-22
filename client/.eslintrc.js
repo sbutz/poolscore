5230a9db03990b479ab7ff6dc936d82d70c949e3
Stefan Butz <mail@butz.st>
2023-03-22 15:28:05 +0100

    Move eslint config to own file
    Make package.json smaller.
    Preparation for live formatting in vscode.

---
+++ client/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:react/jsx-runtime",
+    ],
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    rules: {
+        "consistent-return": "off",
+        "no-console": "warn",
+        "no-underscore-dangle": "off",
+        "react/require-default-props": [
+            2,
+            {
+                functions: "defaultArguments",
+            },
+        ],
+    },
+};
