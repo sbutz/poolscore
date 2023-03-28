1bd3197c0179fceb95d3922636399d7cf464a7f3
Stefan Butz <mail@butz.st>
2023-03-28 16:19:49 +0200

    Show and edit club's name

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
 import {
   createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider,
 } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
 
+import { useAuth } from './store/AuthProvider';
 import ErrorPage from './views/ErrorPage';
 import Game from './views/Game';
 import Game14 from './views/Game14';
@@ -13,40 +12,21 @@ import Matchdays from './views/Matchdays';
 import Matchday from './views/Matchday';
 import Login from './views/Login';
 import Register from './views/Register';
-import { auth } from './store/Firebase';
 
 interface RequireProps {
   children: JSX.Element;
 }
 function RequireLogin({ children } : RequireProps) {
-  const [user, loading] = useAuthState(auth);
+  const { userId, userIdLoading } = useAuth();
 
-  if (!user && !loading) return <Navigate to="/login" replace />;
+  if (!userId && !userIdLoading) return <Navigate to="/login" replace />;
 
   return children;
 }
 
-function RequireAdmin({ children } : RequireProps) {
-  const [user] = useAuthState(auth);
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [claimLoading, setClaimLoading] = useState(true);
-
-  useEffect(() => {
-    if (!user) return;
-    user.getIdTokenResult().then((result) => {
-      setClaimLoading(false);
-      if (result.claims.role && result.claims.role === 'admin') setIsAdmin(true);
-    });
-  }, [user]);
-
-  if (claimLoading) return null;
-  if (!isAdmin) return <Navigate to="/login" replace />;
-
-  return <RequireLogin>{children}</RequireLogin>;
-}
-
 export default function Router() {
   const router = createBrowserRouter(
+    /* Keep in Sync with AppDrawer */
     createRoutesFromElements(
       <>
         <Route path="/" element={<Navigate to="/game" replace />} />
@@ -54,10 +34,10 @@ export default function Router() {
         <Route path="/game14" element={<Game14 />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/club" element={<RequireAdmin><Club /></RequireAdmin>} />
-        <Route path="/tables" element={<RequireAdmin><Tables /></RequireAdmin>} />
-        <Route path="/matchday" element={<RequireAdmin><Matchdays /></RequireAdmin>} />
-        <Route path="/matchday/:id" element={<RequireAdmin><Matchday /></RequireAdmin>} />
+        <Route path="/club" element={<RequireLogin><Club /></RequireLogin>} />
+        <Route path="/tables" element={<RequireLogin><Tables /></RequireLogin>} />
+        <Route path="/matchday" element={<RequireLogin><Matchdays /></RequireLogin>} />
+        <Route path="/matchday/:id" element={<RequireLogin><Matchday /></RequireLogin>} />
         <Route path="*" element={<ErrorPage />} />
       </>,
     ),
