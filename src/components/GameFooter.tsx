2cec835c0511728f93a7b8e86d56f220d1db091b
Stefan Butz <mail@butz.st>
2024-07-05 21:30:57 +0200

    Add Matchday Button

--- src/components/GameFooter.tsx
+++ src/components/GameFooter.tsx
@@ -1,4 +1,8 @@
-import { AppBar, Toolbar, Stack } from '@mui/material';
+import { EmojiEvents } from '@mui/icons-material';
+import {
+  AppBar, Toolbar, Stack, Button,
+} from '@mui/material';
+import { Link } from 'react-router-dom';
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -9,7 +13,19 @@ export default function GameFooter({ children }: HeaderProps) {
     <AppBar color="transparent" position="sticky">
       <Toolbar>
         <Stack direction="row" justifyContent="center" width="100%" spacing={3}>
-          {children}
+          <Button
+            color="inherit"
+            startIcon={<EmojiEvents />}
+            component={Link}
+            to="/matchdays"
+            sx={{ ml: 4, position: 'fixed', left: 0 }}
+            disabled
+          >
+            Spieltage
+          </Button>
+          <Stack direction="row" spacing={3}>
+            {children}
+          </Stack>
         </Stack>
       </Toolbar>
     </AppBar>
